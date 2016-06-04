e0d570dd6333f7fc95892a2f4d943140b5968ec1
Helga Gebremariam <helga.gebremariam@gmail.com>
2016-06-04 14:42:41 +0300

    Exponential Sequence Generator

---
+++ TestSystem.TestGenerator/ExponentialSequenceGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSystem.TestGenerator.Interfaces;
+
+namespace TestSystem.TestGenerator
+{
+    public class ExponentialSequenceGenerator : SequenceGenerator
+    {
+        public ExponentialSequenceGenerator(IRandomGenerator randomGenerator) : base(randomGenerator)
+        {
+        }
+
+        public double[] GenerateSequence()
+        {
+            double[] sequence = base.GenerateSequence();
+            for (int i = 0; i < SequenceLength; i++)
+            {
+                sequence[i] = -MathematicalExpectation*Math.Log(sequence[i]);
+            }
+            return sequence;
+        }
+    }
+}
